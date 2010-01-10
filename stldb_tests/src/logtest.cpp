7225ca3f78eba7f772dc219bc25da693613d9600
Bob Walters <bobs.s.walters@gmail.com>
2010-01-10 22:41:26 +0000

    finishing test
    git-svn-id: https://svn.code.sf.net/p/stldb/code/branches/r1.2@317 76cca37e-3e0e-4c71-b5a1-d872068be9f0

--- stldb_tests/src/logtest.cpp
+++ stldb_tests/src/logtest.cpp
@@ -18,13 +18,15 @@ int main(int argc, const char* argv[])
   int trace_level = properties.getProperty("tracing", (int)stldb::fine_e);
   stldb::tracing::set_trace_level( (stldb::trace_level_t)trace_level );
 
+  // directory to write the log file into.
+  std::string default_dir( "." );
+  std::string log_dir = properties.getProperty("log_dir", default_dir);
+
   // buffer size
   int buffer_size = properties.getProperty("buffer_size", 0);
   // number of buffers to put into the queue before making the same # of log() calls
   // only on of the log calls will write (doing all buffers by aggregation.)
   int aggregation = properties.getProperty("aggregation", 1);
-  // device block size for write alignment.
-  int block_size = properties.getProperty("block_size", 512);
 
   // how many buffers to write during the timed test.
   int loopsize = properties.getProperty("loop_size", 1000);
@@ -35,10 +37,18 @@ int main(int argc, const char* argv[])
   // should fsync() be done after a write?
   int sync_write = properties.getProperty("sync", true);
   
+  // device block size for write alignment.
+//  int block_size = properties.getProperty("block_size", 512);
+  // Set blocksize.
+//  stldb::Logger<std::allocator<void>, boost::interprocess::interprocess_mutex>.optimum_write_alignment = block_size;
+
   std::allocator<void> alloc;
   stldb::SharedLogInfo<std::allocator<void>, boost::interprocess::interprocess_mutex>
   	shared_info(alloc);
-  	
+  shared_info.log_dir= log_dir.c_str();
+  shared_info.log_max_len = log_max_len;
+  shared_info.log_sync = sync_write;
+
   stldb::Logger<std::allocator<void>, boost::interprocess::interprocess_mutex> logger;
   logger.set_shared_info( &shared_info );
   
@@ -48,18 +58,29 @@ int main(int argc, const char* argv[])
   std::vector<commit_buffer_t*> buffers;
   
   for (int j=0; j<aggregation; j++) {
-	  buffers.push_back( new commit_buffer_t(alloc) );
-	  // TODO - add buffer_size bytes to the buffer
-  }
-  for (int i=0; i<loopsize; i++) {
-  	for (int j=0; j<aggregation; j++) {
-  		logger.queue_for_commit( buffers[j] );
-  	}
-  	for (int j=0; j<aggregation; j++) {
-  		logger.log( i*aggregation + j);
-  	}
+    commit_buffer_t *buff = new commit_buffer_t(alloc);
+    buff->reserve(buffer_size);
+    for (int k=0; k<buffer_size; k++) {
+      buff->push_back(k);
+    }
+    assert(buff->size() == buffer_size);
+    buff->op_count = 10;
+    buffers.push_back( buff );
   }
 
+  loopsize /= aggregation;
+  stldb::transaction_id_t commit_id[aggregation];
+  for (int i=0; i<loopsize; i++) {
+  	for (int j=0; j<aggregation; j++) {
+  	    commit_id[j] = logger.queue_for_commit( buffers[j] );
+  	}
+  	for (int j=0; j<aggregation; j++) {
+  	    logger.log( commit_id[j] );
+  	}
+  }
+  if (stldb::timer::enabled) {
+    stldb::time_tracked::print(std::cout, true);
+  }
 }
 
 
