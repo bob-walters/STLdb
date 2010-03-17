008087b12673419fb28c4fbc2b4175ced107ea47
Bob Walters <bobs.s.walters@gmail.com>
2010-03-17 19:49:39 +0000

    adding more config around use of timing
    git-svn-id: https://svn.code.sf.net/p/stldb/code/branches/r1.2@374 76cca37e-3e0e-4c71-b5a1-d872068be9f0

--- stldb_lib/src/timer.cpp
+++ stldb_lib/src/timer.cpp
@@ -7,11 +7,21 @@
 
 #define BOOST_STLDB_SOURCE
 
+#include <stldb/stldb.hpp>
+
 #include <stldb/timing/timer.h>
 
 namespace stldb
 {
 
-bool timer::enabled = false;
+// Current timer configuration (frequency, report interval, etc.)
+timer_configuration BOOST_STLDB_DECL timer::config;
+
+// Is time tracking current on for the current thread.
+boost::thread_specific_ptr<bool> BOOST_STLDB_DECL timer::thr_enabled;
+
+// date/time of the last dump of accumulated timer data.
+boost::posix_time::ptime BOOST_STLDB_DECL timer::last_report_time;
+
 
 }
